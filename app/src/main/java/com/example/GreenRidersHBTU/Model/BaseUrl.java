c9fb1aee27e0dbfdaced54c1edd8b1da2af4f617
harcourtian <suryanshhbtu@gmail.com>
2023-04-06 00:19:08 +0530

    first commit

---
+++ app/src/main/java/com/example/GreenRidersHBTU/Model/BaseUrl.java
@@ -0,0 +1,9 @@
+package com.example.GreenRidersHBTU.Model;
+
+public class BaseUrl {
+    private String baseUrl = "https://app.hbtu.ac.in/";
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+}
---
+++ app/src/main/java/com/example/GreenRidersHBTU/Util/AppInfo.java
@@ -0,0 +1,27 @@
+package com.example.GreenRidersHBTU.Util;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.GreenRidersHBTU.R;
+
+
+public class AppInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // back button to close this activity
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        setContentView(R.layout.activity_app_info);
+    }
+    // back button to close Home
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish(); // close this activity as oppose to navigating up
+        return false;
+    }
+}
